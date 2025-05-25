a137a859e5f65936d65a38682f56cbbe4e0562c1
leonardcwh2731 <leonardcwh2731@gmail.com>
2025-05-25 23:29:02 +0800

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,9 @@
 // pages/login.tsx
 export default function Login() {
-  const clientId = process.env.858807355864-j4q8g0sm8ncspjqr1u6gqps3dkt0o6c2.apps.googleusercontent.com;
+  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
   const redirectUri = 'https://saveyouraudience.com/api/auth/callback';
 
-  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=$858807355864-j4q8g0sm8ncspjqr1u6gqps3dkt0o6c2.apps.googleusercontent.com
-&redirect_uri=$https://saveyouraudience.com/api/auth/callback&response_type=code&scope=openid%20email%20profile%20https://www.googleapis.com/auth/gmail.readonly%20https://www.googleapis.com/auth/calendar.readonly&access_type=offline&prompt=consent`;
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=openid%20email%20profile%20https://www.googleapis.com/auth/gmail.readonly%20https://www.googleapis.com/auth/calendar.readonly&access_type=offline&prompt=consent`;
 
   return (
     <div style={{ textAlign: 'center', marginTop: '20px' }}>
